1d2edd65963671428cf0d4c9ca8f05be30ea8f05
Ivan <ivan@MASAs-MacBook-Pro.local>
2022-02-08 22:46:44 +0300

    delete message

--- index.py
+++ index.py
@@ -51,10 +51,9 @@ markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True)
 
 def facts_to_str(user_data: Dict[str, str]) -> str:
     """Helper function for formatting the gathered user info."""
-    if 'Dop_file' in user_data:
-        del user_data['Dop_file']
     facts = [f'{key} - {value}' for key, value in user_data.items()]
-    return "\n".join(facts).join(['\n', '\n'])
+    filter_facts = list(filter(lambda score: 'Dop_file' not in score ,facts))
+    return "\n".join(filter_facts)
 
 def start(update: Update, context: CallbackContext) -> int:
     message = update.effective_message
@@ -99,7 +98,7 @@ def received_information(update: Update, context: CallbackContext) -> int:
     del user_data['choice']
     # del user_data['login']
     update.message.reply_text(
-        f"""Для информации вы указали следущию информацию
+        f"""Для информации вы указали следущию информацию:
 {facts_to_str(user_data)}
 Укажите больше или измените если ошиблись.
 Для завершения заявки нашмите "Отправить" """,
@@ -109,24 +108,26 @@ def received_information(update: Update, context: CallbackContext) -> int:
     return CHOOSING
 
 def done(update: Update, context: CallbackContext) -> int:
-    """Display the gathered info and end the conversation."""
     chat = '-1001698024013'
+    id = uuid.uuid4()
     user_data = context.user_data
     if 'choice' in user_data:
         del user_data['choice']
     if 'login' in user_data:
         del user_data['login']
     update.message.reply_text(
-        f"Следующая заявка сформирована: {facts_to_str(user_data)}\nСпасибо Мы свяжимся с вами в ближайшее время!",
+        f"Следующая заявка сформирована:\nID:{id}\n{facts_to_str(user_data)}\nСпасибо Мы свяжимся с вами в ближайшее время!",
         reply_markup=ReplyKeyboardRemove(),
     )
-    context.bot.send_message(chat, f"Ticket {uuid.uuid4()}: {facts_to_str(user_data)}")
+    context.bot.send_message(chat, f"Ticket {id}: {facts_to_str(user_data)}")
+    print(user_data)
     if 'Dop_file' in user_data:
         dop_files = user_data['Dop_file'].split()
+        print(dop_files)
         for dop_file in dop_files:
+            # context.bot.send_document(chat, dop_file)
             context.bot.copy_message(chat, update.message.chat_id, dop_file)
         del user_data['Dop_file']
-    
     user_data.clear()
     return ConversationHandler.END
 
@@ -174,27 +175,29 @@ def help_choice(update: Update, context: CallbackContext) -> int:
     return TYPING_REPLY
 
 def problem_choice(update: Update, context: CallbackContext) -> int:
+    message = update.effective_message
     text = update.message.text
     context.user_data['choice'] = text 
     update.message.reply_text(
         f"Добавте контактную информацию для обратной связи:",
         reply_markup=ReplyKeyboardMarkup(problem_keyboard, one_time_keyboard=True),
     )
+    context.bot.delete_message(message.chat_id, message.message_id)
     return TYPING_REPLY
 
 def dop_choice(update: Update, context: CallbackContext) -> int:
+    message = update.effective_message
     text = update.message.text
     context.user_data['choice'] = text 
     update.message.reply_text(
         f"Добавте фото или мануал который поможет быстрее решить проблему",
         reply_markup=ReplyKeyboardMarkup([], input_field_placeholder='Добавте документ', one_time_keyboard=True)
     )
+    context.bot.delete_message(message.chat_id, message.message_id)
     return TYPING_DOP
 
 def dop_recive(update: Update, context: CallbackContext) -> None:
-    chat = '-1001698024013'
     message = update.effective_message
-    text = update.message.text
     #Download photo
     # file_data = update.message.photo[-1].get_file()
     # file_data.download('photo.jpg')
@@ -203,9 +206,9 @@ def dop_recive(update: Update, context: CallbackContext) -> None:
     # file_data.download(update.message.document.file_name)
     user_data = context.user_data
     try:
-        user_data['Dop_file'] = f"{user_data['Dop_file']} {update.effective_message.message_id}"
+        user_data['Dop_file'] = f"{user_data['Dop_file']} {message.message_id}"
     except Exception as e:
-        user_data['Dop_file'] = update.effective_message.message_id
+        user_data['Dop_file'] = message.message_id
     update.message.reply_text(
         f"Файл добавлен!",
         reply_markup=markup
